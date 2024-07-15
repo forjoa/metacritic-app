67eada9d513da5bcb10f9a6eeed29fa64b6a7235
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-07-15 18:22:09 +0200

    build: install eslint

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableHighlight,
+} from 'react-native'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>te amo muchismuchis!</Text>
-      <StatusBar style='auto' />
+      <StatusBar style='light' />
+      <Image
+        source={{
+          uri: 'https://www.metacritic.com/a/img/catalog/provider/6/3/6-1-4763-13.jpg',
+        }}
+        style={styles.image}
+      />
+      <Text style={styles.text}>zeldaa!</Text>
+      <Button title='Click' onPress={() => alert('Hola')} />
+      <TouchableHighlight
+        underlayColor={'#09f'}
+        onPress={() => alert('hola')}
+        style={{ padding: 10, backgroundColor: '#07d', borderRadius: 10 }}
+      >
+        <Text style={styles.text}>clickkkk</Text>
+      </TouchableHighlight>
     </View>
   )
 }
@@ -13,8 +34,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    color: 'white',
+  },
+  image: {
+    width: 215,
+    height: 294,
   },
 })
